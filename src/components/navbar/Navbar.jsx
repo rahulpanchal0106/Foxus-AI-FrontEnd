4de5142ffcbd4a6b1e3aca534da8194bb29f3dad
Rahul Panchal <rm5901960@gmail.com>
2024-04-12 17:50:49 +0530

    Add login detection based on cookie

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,26 +2,39 @@
 import styles from "./navbar.module.css"
 import { Link, NavLink } from "react-router-dom"
 import ThemeToggle from "../themeToggle/ThemeToggle"
+import { useState,useEffect } from "react"
+import Cookies from 'js-cookies'
 
-
-export const Navbar = () => {
+export const Navbar = (logged) => {
+  const [loginStatus,setLoginStatus] = useState(false);
+  function handleLoginStatus(){
+    const token = Cookies.getItem('token');
+    return setLoginStatus(token||logged?true:false)
+  }
+  useEffect(() => {
+    handleLoginStatus();
+  }, []);
+  
   return (
     <div className={styles.container}>
       <div className={styles.social}>
-        <imag src="/instagram.png" alt="instagram" width={24} height={24}/>
+      <img src="/instagram.png" alt="instagram" width={24} height={24}/>
+
       </div>
       <Link to="/" className={styles.logo}>
       <div >AI Learner</div>
       </Link>
       <div className={styles.links}>
+
         <ThemeToggle  />
         
-          <NavLink to="/register"  className={styles.navLink}>Sign Up</NavLink>
         
-        
+        <NavLink to="/register"  className={styles.navLink}>Sign Up</NavLink>
+        {
+          loginStatus?
+          <NavLink to="/logout" className={styles.navLink}>Log Out</NavLink>:
           <NavLink to="/login" className={styles.navLink}>Log in</NavLink>
-        
-        
+        }
       </div>
     </div>
   )

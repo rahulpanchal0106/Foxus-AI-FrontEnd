770425c225793f4fd5a89ca2a4a9e16b9c81a9b3
Jay100125 <jaypatel100125@gmail.com>
2024-04-20 14:11:44 +0530

    FIX

--- src/components/layer2Card/Layer2Card.jsx
+++ src/components/layer2Card/Layer2Card.jsx
@@ -2,20 +2,30 @@ import React, { useState } from "react";
 import "./layer2Card.css";
 import Layer3 from "../../page/layer3/Layer3";
 
-const Layer2Card = ({ lessonName, lessonContent, chapter, level, subject }) => {
+const Layer2Card = ({
+  lessonName,
+  lessonContent,
+  chapter,
+  level,
+  subject,
+  setActiveCardIndex,
+  activeCardIndex,
+  index,
+}) => {
   const [showLayer3, setShowLayer3] = useState(false);
 
   const handleClick = () => {
     setShowLayer3(!showLayer3);
+    setActiveCardIndex(index);
   };
 
   return (
     <div className="layer-container">
-      <div className="layer2-card" onClick={handleClick}>
+      <div className={`layer2-card ${showLayer3 && "active"}`} onClick={handleClick}>
         <h3>{lessonName}</h3>
         <p>{lessonContent}</p>
       </div>
-      {showLayer3 && (
+      {showLayer3 && activeCardIndex === index && (
         <div className="layer3-content">
           <Layer3
             lessonName={lessonName}

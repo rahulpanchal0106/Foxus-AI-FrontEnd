a930c30387f0349e3686abe8dc9e06fbbeb9e597
Jay100125 <jaypatel100125@gmail.com>
2024-05-26 12:14:33 +0530

    media

--- src/components/ui/typewriter/TypeWrite.jsx
+++ src/components/ui/typewriter/TypeWrite.jsx
@@ -133,7 +133,7 @@ export const TypewriterEffectSmooth = ({
         }}
       >
         <div
-          className="text-xs sm:text-base md:text-xl lg:text:3xl xl:text-5xl font-bold"
+          className="text-base sm:text-2xl md:text-3xl lg:text-5xl xl:text-5xl font-bold"
           style={{
             whiteSpace: "nowrap",
           }}
@@ -154,7 +154,7 @@ export const TypewriterEffectSmooth = ({
           repeatType: "reverse",
         }}
         className={cn(
-          "block rounded-sm w-[4px]  h-4 sm:h-6 xl:h-12 bg-blue-500",
+          "block rounded-sm w-[4px]  h-5 sm:h-6 md:h-8 lg:h-10 xl:h-12 bg-blue-500",
           cursorClassName
         )}
       ></motion.span>

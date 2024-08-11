f5a48c8b3cde2b7172e886cb4bb7b220117b326d
Rahul Panchal <rm5901960@gmail.com>
2024-08-11 14:27:35 +0530

    History disabled

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ const App = () => {
         setLoading(true);
         console.log(isLoading);
         const response = await fetch(
-          "https://foxus-ai-be.onrender.com/activity",  //https://foxus-ai-be.onrender.com
+          "/activity",  //https://foxus-ai-be.onrender.com
           {
             method: "GET",
             headers: {
